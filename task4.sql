24d9f4dc1471bfafc2ec48dc86f49984bc35c4f1
ArsenK391 <arsenkazhybayev@gmail.com>
2024-09-17 18:14:33 +0500

    Delete task4.sql

--- task4.sql
+++
@@ -1,9 +0,0 @@
-UPDATE users
-SET isadmin_temp = CASE
-    WHEN isadmin = 1 THEN true
-    ELSE false
-END
-
-ALTER TABLE users DROP COLUMN isadmin
-
-ALTER TABLE users RENAME COLUMN isadmin_temp TO isadmin
